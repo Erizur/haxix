9e6a787c5448fe695b812cd71d2b9923c5bb1451
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2025-05-25 20:49:41 -0500

    Update generic.nix

--- pkgs/lime/generic.nix
+++ pkgs/lime/generic.nix
@@ -4,8 +4,8 @@
 { lib, haxe, ... }:
 haxe.buildHaxelib {
   pname = "lime";
-  version = "8.1.3";
-  sha256 = "sha256-bOwe+jNymk+5liOv0eoFlNtrYxLBlxivntFnrQDDgKU=";
+  version = "8.2.2";
+  sha256 = "sha256-ZdwhgCIqzsgSeViCITLk71ouOTIANzY/082ibLa1As0=";
   meta = {
     homepage = "https://lime.openfl.org/";
     description = "A foundational Haxe framework for cross-platform development";
